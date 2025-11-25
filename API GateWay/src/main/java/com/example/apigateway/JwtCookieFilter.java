8ddbf554143646f88e236d03b47dbc64fefbb545
Pancham-Ayush <panchamayush@gmail.com>
2025-11-25 17:36:19 +0530

    remove test file

--- API GateWay/src/main/java/com/example/apigateway/JwtCookieFilter.java
+++ API GateWay/src/main/java/com/example/apigateway/JwtCookieFilter.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
